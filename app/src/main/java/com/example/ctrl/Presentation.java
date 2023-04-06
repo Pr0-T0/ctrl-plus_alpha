0016ba62aa25226cab7e878a778c9c85cede5635
Pr0-T0 <sarathsasidharan557@gmail.com>
2023-04-06 20:30:07 +0530

    presertation timer

--- app/src/main/java/com/example/ctrl/Presentation.java
+++ app/src/main/java/com/example/ctrl/Presentation.java
@@ -1,16 +1,33 @@
 package com.example.ctrl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Presentation extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 60000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
     Button start, left, right;
     SharedPreferences preferences;
     String ipAddress;
@@ -18,6 +35,7 @@ public class Presentation extends AppCompatActivity {
     Handler handler = new Handler();
     boolean isSendingCode = false;
     int currentKeyCode = -1;
+
     Runnable sendCodeRunnable = new Runnable() {
         @Override
         public void run() {
@@ -39,6 +57,20 @@ public class Presentation extends AppCompatActivity {
         preferences = getSharedPreferences("MyPreferences", MODE_PRIVATE);
         ipAddress = preferences.getString("ipAddress", "");
         port =  preferences.getInt("port", 0);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        Toolbar toolbar = findViewById(R.id.toolbar3);
+        toolbar.setTitle(R.string.activity_presentation);
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTimer();
+            }
+        });
+        updateCountDownText();
         start.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -46,7 +78,11 @@ public class Presentation extends AppCompatActivity {
                     currentKeyCode = 116;
                     isSendingCode = true;
                     handler.post(sendCodeRunnable);
-                    System.out.println("116");
+                    if(mTimerRunning){
+                        pauseTimer();
+                    }else{
+                        startTimer();
+                    }
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     isSendingCode = false;
                 }
@@ -62,7 +98,6 @@ public class Presentation extends AppCompatActivity {
                     isSendingCode = true;
 
                     handler.post(sendCodeRunnable);
-                    System.out.println("37");
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     isSendingCode = false;
                 }
@@ -78,12 +113,47 @@ public class Presentation extends AppCompatActivity {
                     isSendingCode = true;
 
                     handler.post(sendCodeRunnable);
-                    System.out.println("39");
                 } else if (event.getAction() == MotionEvent.ACTION_UP) {
                     isSendingCode = false;
                 }
                 return true;
             }
         });
+    }
+    private void startTimer(){
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        start.setText("pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        start.setText("start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+    private void resetTimer(){
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
     }
 }
